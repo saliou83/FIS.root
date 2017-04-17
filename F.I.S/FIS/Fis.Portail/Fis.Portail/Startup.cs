d188808fc778ccf9bbc0ea71f73207c38f565918
saliou83 <fallsaliou23@gmail.com>
2017-04-17 19:59:13 +0200

    SFA : Ajout de FIS PORTAIL

---
+++ F.I.S/FIS/Fis.Portail/Fis.Portail/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fis.Portail.Startup))]
+namespace Fis.Portail
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
