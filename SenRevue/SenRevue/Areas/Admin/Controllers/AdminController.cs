e57a4133861bd1fd25a697e9b0979ba69908c3e6
saliou83 <fallsaliou23@gmail.com>
2017-08-26 20:08:04 +0200

    Création du menu de l'admin

--- SenRevue/SenRevue/Areas/Admin/Controllers/AdminController.cs
+++ SenRevue/SenRevue/Areas/Admin/Controllers/AdminController.cs
@@ -19,5 +19,29 @@ namespace SenRevue.Areas.Admin.Controllers
             model.Title = LabelHelpers.GetLabel("admin_home_title");
             return View(model);
         }
+
+        /// <summary>
+        /// Page des articles
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult News()
+        {
+            var model = new AdminNewsViewModel();
+            model.Title = LabelHelpers.GetLabel("article_label");
+            return View(model);
+        }
+
+        /// <summary>
+        /// Page des rubriques
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Rubrics()
+        {
+            var model = new AdminRubricsViewModel();
+            model.Title = LabelHelpers.GetLabel("rubrique_label");
+            return View(model);
+        }
     }
 }
---
+++ SenRevue/SenRevue/Areas/Admin/Models/ViewModel/AdminRubricsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SenRevue.Areas.Admin.Models.ViewModel
+{
+    public class AdminRubricsViewModel: AdminViewModelBase
+    {
+        public AdminRubricsViewModel()
+        {
+        }
+    }
+}
